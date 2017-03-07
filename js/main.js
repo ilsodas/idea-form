cc2d3fe36dca0d9c462bec000433b4a9d11a4642
Ilsoda Musa Mbungele <ilsoda@applogix.co.za>
2017-03-07 23:41:46 +0200

    Save form & validations

--- js/main.js
+++ js/main.js
@@ -1,2 +1,83 @@
+$(document).ready(function(){
+    var errors = false;
+    $('.required').parent().find('.input').on('blur', function() {
+        var error_div = $(this).parent().find('.ErrorMessage');
+        var field_container = $(this).parent();
+        if (!$.empty_field_validation($(this).val())) {
+            error_div.html('This field is required.');
+            error_div.css('display', 'block');
+            field_container.addClass('error');
+			errors = true;
+        } else {
+            error_div.html('');
+            error_div.css('display', 'none');
+            field_container.removeClass('error');
+			errors = false;
+        }
+    });
+    $('#Email').on('blur', function(){
+        var error_div = $(this).parent().find('.ErrorMessage');
+        var field_container = $(this).parent();
+        if (!$.EmailValidate($(this).val())) {
+            error_div.html('Please enter Email, this can not be empty.');
+            error_div.css('display', 'block');
+            field_container.addClass('error');
+			errors = true;
+        } else {
+            error_div.html('');
+            error_div.css('display', 'none');
+            field_container.removeClass('error');
+			errors = false;
+        }
+    });
+	$('#IdeaCaptureForm').submit(function(event) {
+		event.preventDefault();
+		 $('.required').parent().find('.input').trigger('blur');
+        if (!errors)
+            $.ajax({
+                url: 'https://httpbin.org/post',
+                data: {
+                    json: JSON.stringify($(this).serializeObject())
+                },
+                type: 'post',
+                success: function(data) {
+                    var message = 'Hi '+data.Email+'. Your message was sent and received.';
+                    $('#after_submit').html(message);
+                    $('#after_submit').css('display', 'block');
+                },
+                error: function() {
+                    var message = 'Hi '+data.Email+'. Your message could not be sent or received. Please try again later';
+                    $('#after_submit').html(message);
+                    $('#after_submit').css('display', 'block');
+                }
+            });
+		else
+			alert("You didn't completed the form correctly. Check it out and try again!");
+	});
+});
 
-alert("helo");
+$.empty_field_validation = function(field_value) {
+    if (field_value.trim() == '') return false;
+    return true;
+}
+
+$.EmailValidate = function(Email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(Email);
+}
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
